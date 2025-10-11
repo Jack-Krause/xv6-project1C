36dcf8e6b0421a438afc2f3a46c390219a9f6786
jmkrause <jmkrause@pyrite-n5.cs.iastate.edu>
2025-10-10 22:14:39 -0500

    2.5 additions

--- user/logtest.c
+++ user/logtest.c
@@ -2,11 +2,25 @@
 #include "kernel/stat.h"
 #include "user/user.h"
 
-int main(int arg, char *argv[]) {
+int main(int argc, char *argv[]) {
     // TODO...
 
+    startLogging();
+    int pid = fork();
+    if (pid < 0) {
+        printf("logtest: fork failed\n");
+        stopLogging();
+        exit(1);
+    }
 
+    if (pid == 0) {
+       int child_pid = getpid();
 
+       int newnice = nice(child_pid, 10);
+       // do cpu burn
+    }
+
+    stopLogging();
     exit(0);
 }
 
