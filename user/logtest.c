b1c3f945c8bb9b978f1c53c592d00cfcc8523bd7
jmkrause <jmkrause@pyrite-n5.cs.iastate.edu>
2025-10-10 21:55:12 -0500

    2.5 imports and main for logtest.c

---
+++ user/logtest.c
@@ -0,0 +1,26 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int main(int arg, char *argv[]) {
+    // TODO...
+
+
+
+    exit(0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
